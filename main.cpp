ff253361fb3def493513942003ead537774d8469
givikuna <thommaster10@gmail.com>
2019-03-21 21:07:57 -0400

    Start Qt lessons on GIT

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    char f = 103;
+    char b = 105;
+    char c = 118;
+    cout << f<<b<<c<<b << endl;
+    cout << "-----" << endl;
+    char m = 1;
+    char y = 2;
+    //space
+    char n = 3;
+    char a = 4;
+    //m _ 4
+    char e = 6;
+    //space
+    char i = 7;
+    char s = 8;
+    //space
+    char g = 9;
+    //i _ 10
+    char v = 11;
+    //i _ 12
+
+    cout<< m<<y<<n<<a<<m<<e<<i<<s<<g<<i<<v<<i <<endl;
+
+    return 0;
+}
