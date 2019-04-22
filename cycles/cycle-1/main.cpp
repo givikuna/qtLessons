942f3d4f58c79e13b6f20398d18595d29db9984e
givikuna <thommaster10@gmail.com>
2019-04-21 21:19:42 -0400

    cycle_lesson_1

---
+++ cycles/cycle-1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    for (int i=0;i<10;i++)
+    {
+     arr[i] = 5;
+     cout<<arr[i]<<endl;
+    }
+
+    //
+
+    cout<<"\n\r";
+
+    //
+
+    int arr1[10];
+
+    for (int l=0;l<10;l++)
+    {
+        arr1[l] = l - ;
+    }
+
+    //
+
+    cout<<"\n\r";
+
+    //
+
+    for (int l=0;l<10;l++)
+    {
+        cout<<arr1[l]<<endl;
+    }
+
+    // initialize
+    return 0;
+}
