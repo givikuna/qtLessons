fbc4ba44cc09d70de7c5c00e894e74622b64552a
givikuna <thommaster10@gmail.com>
2019-04-24 22:01:31 -0400

    entering-1_lesson_1

---
+++ entering-1/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    //cout << "enter any number: ";
+
+    cout<<"ENTER two different numbers and then they will be added, multyplied, divided, or subtracted"<<endl;//kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+
+    cout<<"addition"<<endl;
+
+    cout<<"enter first number ";
+    cin>>k;
+    cout<<"your first number is: "<<k<<endl;
+    int b;
+    cout<<"enter second number ";
+    cin>>b;
+    cout<<"your second number is: "<<b<<endl;
+
+    int c;
+    c = k + b;
+
+    cout<<"first + second = "<<c<<endl;
+
+    //
+
+    cout<<"\n\r";
+
+    //
+
+    cout<<"subtraction"<<endl;
+
+    cout<<"enter first number ";
+    cin>>k;
+    cout<<"your first number is: "<<k<<endl;
+
+    cout<<"enter second number ";
+    cin>>b;
+    cout<<"your second number is: "<<b<<endl;
+
+    c = k - b;
+    cout<<"first - second = "<<c<<endl;
+
+    //
+
+    cout<<"\n\r";
+
+    //
+
+    cout<<"multiply"<<endl;
+
+    cout<<"enter first number ";
+    cin>>k;
+    cout<<"your first number is: "<<k<<endl;
+
+    cout<<"enter second number ";
+    cin>>b;
+    cout<<"your second number is: "<<b<<endl;
+
+    c = k * b;
+    cout<<"first * second = "<<c<<endl;
+
+    //
+
+    cout<<"\n\r";
+
+    //
+
+    cout<<"divide"<<endl;
+
+    cout<<"enter first number ";
+    cin>>k;
+    cout<<"your first number is: "<<k<<endl;
+
+    cout<<"enter second number ";
+    cin>>b;
+    cout<<"your second number is: "<<b<<endl;
+
+    c = k / b;
+    cout<<"first / second = "<<c<<endl;
+
+    return 0;
+}
