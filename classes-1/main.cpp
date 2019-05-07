c9745cf60c1d45c12712205370c61c6ad8bb93c6
givikuna <thommaster10@gmail.com>
2019-05-06 21:22:33 -0400

    Calculator_in_classes

--- classes-1/main.cpp
+++ classes-1/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -27,6 +28,7 @@ struct Calculator
 
 int main()
 {
+/*
     Calculator calc;
     cout<<calc.add(10,5)<<endl;
     cout<<calc.deduct(10,5)<<endl;
@@ -34,9 +36,140 @@ int main()
     cout<<calc.divide(10,5)<<endl;
 
     calc.m_member1 = 100;
-    calc.m_member2 = 9.789;
+    calc.m_member2 = 9789;
 
     cout<<calc.m_member1<<endl;
     cout<<calc.m_member2<<endl;
+
+    cout<<"+";
+    cin>>a;
+
+    cout<<"+";
+    cin>>b;
+
+    cout<<"+"<<calc.add(a,b)<<endl;
+
+    //
+
+    */
+
+    Calculator calc;
+
+    for ( ; ; ){
+
+        char go;
+
+        cout<<"what do you want to do '+', '-', '*',  '/' : ";
+        cin>>go;
+
+        int a,b;
+
+        if(go == '+'){
+            cout<<"-------------------------------------------"<<endl;
+            cout<<"                   You are adding"<<endl;
+
+            //
+
+            cout<<"write your first number  _  ";
+            cin>>a;
+            cout<<"your first number is = "<<a<<endl;
+
+            cout<<"write your second number  _  ";
+            cin>>b;
+            cout<<"yout second number is = "<<b<<endl;
+
+            cout<<a<<" + "<<b<<" = "<<calc.add(a,b)<<endl;
+           }
+
+        else if (go == '-') {
+            cout<<"-------------------------------------------"<<endl;
+            cout<<"             You are subtracting           "<<endl;
+
+            //
+
+            cout<<"write your first number  _  ";
+            cin>>a;
+            cout<<"your first number is = "<<a<<endl;
+
+            cout<<"write your second number  _  ";
+            cin>>b;
+            cout<<"your second number is = "<<b<<endl;
+
+            cout<<a<<" - "<<b<<" = "<<calc.deduct(a,b)<<endl;
+
+        }
+
+        else if (go == '*') {
+            cout<<"-------------------------------------------"<<endl;
+            cout<<"             You are multiplying           "<<endl;
+
+            //
+
+            cout<<"write your first number  _  ";
+            cin>>a;
+            cout<<"your first number is = "<<a<<endl;
+
+            cout<<"write your second number  _  ";
+            cin>>b;
+            cout<<"your second number is = "<<b<<endl;
+
+            cout<<a<<" * "<<b<<" = "<<calc.multi(a,b)<<endl;
+
+        }
+
+        else if (go == '/') {
+            cout<<"-------------------------------------------"<<endl;
+            cout<<"             You are divide                "<<endl;
+
+            //
+
+            cout<<"write your first number  _  ";
+            cin>>a;
+            cout<<"your first number is = "<<a<<endl;
+
+            cout<<"write your second number  _  ";
+            cin>>b;
+            cout<<"your second number is = "<<b<<endl;
+
+            cout<<a<<" / "<<b<<" = "<<calc.divide(a,b)<<endl;
+        }
+        else {
+            cout<<"you entered a wrng sumbol, try again!"<<endl;
+            continue;
+        }
+
+        bool checkBreak = false;//
+        bool checkContinue = false;//
+        for( ; ; )
+        {
+            char confirm;
+            cout<<"do you want to continue?  y/n :  ";
+            cin>>confirm;
+
+            if(confirm == 'y'){
+                checkContinue = true;
+                break;
+            }
+            else if (confirm == 'n') {
+                checkBreak = true;
+                break;
+            }
+            else {
+                cout<<"---"<<endl;
+                cout<<"you entered wrong symbol, try again : "<<endl;
+            }
+        }
+
+        if(checkBreak == true){
+            break;
+        }
+        if(checkContinue == true){
+            continue;
+        }
+
+        //
+
+    }
+
     return 0;
 }
