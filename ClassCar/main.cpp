ce1fc88cf7469ac94337f10b48877306708ebfd4
givikuna <thommaster10@gmail.com>
2019-05-08 22:29:44 -0400

    ClassCar_explaining_lesson-1

---
+++ ClassCar/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+struct Car
+{
+public:
+    Car(){ //constructor
+        m_currentSpeed = 0;
+         cout<<"started speed is: "<<m_currentSpeed<<endl;
+    }
+    // member functions / procedures
+    int speed()
+    {
+        return m_currentSpeed;
+    }
+    void pushBreak()
+    {
+        m_currentSpeed = 0;
+        cout<<"break down and speed is: "<<m_currentSpeed<<endl;
+    }
+    void speedUp()
+    {
+        m_currentSpeed += 1;
+        cout<<"current speed is: "<<m_currentSpeed<<endl;
+
+    }
+    // properties / members
+    int m_currentSpeed;
+    int m_maxSpeed;
+    int m_doors;
+};
+
+int main()
+{
+    Car ferary;
+    for(;;)
+    {
+        char confirm;
+        cout<<"continue speed up? y/n ";
+        cin>>confirm;
+        if(confirm == 'y') {
+            ferary.speedUp();
+        }
+        else {
+            break;
+        }
+        cout<<"break speed? y/n ";
+        cin>>confirm;
+        if(confirm == 'y'){
+            ferary.pushBreak();
+        }
+    }
+    return 0;
+}
