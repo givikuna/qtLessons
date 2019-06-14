95f722be46e82f1b093d976a2474e4d2ede4d5d5
givikuna <thommaster10@gmail.com>
2019-06-14 14:12:02 -0400

    adding_unsaved_files-2

---
+++ array_sort-2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr1[]={55,11,33,22,445,44,45,178,78,346,1};
+
+
+    int max = 10;
+    for (int j=0; j<max; j++)
+    {
+        for (int i=0; i<max; i++)
+        {
+            if ( arr1[i] > arr1[i+1] )
+            {
+                int tmp = arr1[i];
+                arr1[i] = arr1[i+1];
+                arr1[i+1] = tmp;
+            }
+        }
+    }
+
+    for (int i=0; i<max+1;i++) {
+        cout<<arr1[i]<<endl;
+    }
+
+    return 0;
+}
