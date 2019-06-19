6b16c0b03999fa3f6ee098ac00bb4df7b4f63531
givikuna <thommaster10@gmail.com>
2019-06-18 22:27:31 -0400

    Lesson-1

--- array_sort-3/main.cpp
+++ array_sort-3/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int nSize;
     cout<<sizeof(cout)<<endl;
-    cout<<"Enter any number";
+    cout<<"Enter any number _ ";
     cin>>nSize;
     int* pInt = new int[nSize];
     for (int i=0;i<nSize;i++) {
