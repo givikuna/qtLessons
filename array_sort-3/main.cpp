d77a1c9f2859322b8203bdb8a25b93308d6081fd
givikuna <thommaster10@gmail.com>
2019-06-18 22:24:21 -0400

    Lesson-1{DONE}

--- array_sort-3/main.cpp
+++ array_sort-3/main.cpp
@@ -4,41 +4,20 @@ using namespace std;
 
 int main()
 {
-
-    int arr1[11];
-
-    cout<<"Write Down eleven numbers _ ";
-    cin>>arr1[0];
-    cin>>arr1[1];
-    cin>>arr1[2];
-    cin>>arr1[3];
-    cin>>arr1[4];
-    cin>>arr1[5];
-    cin>>arr1[6];
-    cin>>arr1[7];
-    cin>>arr1[8];
-    cin>>arr1[9];
-    cin>>arr1[10];
-
-    cout<<"\n\r";
-
-    int max = 10;
-    for (int j=0; j<max; j++)
-    {
-        for (int i=0; i<max; i++)
-        {
-            if ( arr1[i] < arr1[i+1] )
-            {
-                int tmp = arr1[i];
-                arr1[i] = arr1[i+1];
-                arr1[i+1] = tmp;
-            }
-        }
+    int nSize;
+    cout<<sizeof(cout)<<endl;
+    cout<<"Enter any number";
+    cin>>nSize;
+    int* pInt = new int[nSize];
+    for (int i=0;i<nSize;i++) {
+        pInt[i]=i;
     }
 
-    for (int i=0; i<max+1;i++) {
-        cout<<arr1[i]<<endl;
+    for (int i=0;i<nSize;i++) {
+        cout<<pInt[i]<<endl;
     }
+
+    delete[] pInt;
 
     return 0;
 }
---
+++ keto_test-1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello World!" << endl;
+    //cout << "1" << endl;
+    //cout << "2" << endl;
+
+    int array [10] {1,2,3,4,5,6,7,8,9,10};
+
+    for (int i=0;i<10;i++) {
+        cout<<array[i]<<endl;
+    }
+
+    //
+
+    return 0;
+}
